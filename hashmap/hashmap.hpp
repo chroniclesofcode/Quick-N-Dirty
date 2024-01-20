4e7ec797cc194d7d7024df612edfcdfcd5de3f14
Donald Tang <donald.tang.email@googlemail.com>
2024-01-21 10:15:37 +1100

    passed default tests for hashmap

--- hashmap/hashmap.hpp
+++ hashmap/hashmap.hpp
@@ -1,26 +1,61 @@
 #include <vector>
+#include <iostream>
+#include <optional>
+
+#define likely(expr) __builtin_expect(expr, 1)
+#define unlikely(expr) __builtin_expect(expr, 0)
 
 namespace qnd {
 
-template <typename T, typename U>
+template <typename Key, typename T, typename Hash = std::hash<Key>>
 class hashmap {
 public:
+    using Buckets = std::vector<std::optional<std::pair<Key, T>>>;
     
     hashmap() {
-        buckets = std::vector<T>(size);
+        buckets.resize(size_b);
     }
 
-    std::size_t hash(const T& elem) {
-
+    hashmap(std::size_t size) : size_b{size} {
+        buckets.resize(size_b);
     }
 
-    U& operator[](const T& elem) {
-        
+    inline std::size_t hash(const Key& elem) {
+        return Hash{}(elem) % size_b;
+    }
+
+    void resize() {
+        std::cout << "Resize!\n";
+        size_b *= 2;
+        Buckets tmp(size_b, std::nullopt);
+        swap(buckets, tmp);
+        for (auto e : tmp) {
+            if (e.has_value()) {
+                (*this)[e.value().first] = e.value().second;
+            }
+        }
+    }
+
+    T& operator[](const Key& elem) {
+        if (unlikely(count >= size_b / 4)) {
+            resize();
+        }
+        std::size_t idx = hash(elem);
+        while (buckets[idx].has_value() && buckets[idx].value().first != elem) {
+            ++idx;
+            if (unlikely(idx == size_b)) idx = 0;
+        }
+        if (!buckets[idx].has_value()) {
+            buckets[idx] = std::pair<Key, T>(elem, T{});
+            ++count;
+        } 
+        return buckets[idx].value().second;
     }
 
 private:
-    std::size_t size = 1024;
-    std::vector<U> buckets;
+    std::size_t size_b = 1024;
+    std::size_t count = 0;
+    Buckets buckets;
 };
 
 
--- hashmap/main.cpp
+++ hashmap/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <cassert>
+#include "hashmap.hpp"
+
+void test1() {
+    qnd::hashmap<int, int> mp;
+    // testing modification
+    mp[35] = 200;
+    assert(50 + mp[35] == 250);
+    // testing changing same value
+    mp[35] = 90;
+    assert(mp[35] == 90);
+    // testing large int
+    mp[987654] = 123456;
+    assert(mp[987654] == 123456);
+    int testint = 25 + mp[987654];
+    assert(testint == 123481);
+    // testing ++, as well as map storing multiple values
+    mp[35]++;
+    assert(mp[35] == 91);
+}
 
 int main(void) {
-    std::cout << "Hello world" << std::endl;
+    test1();
+    
     return 0;
 }
